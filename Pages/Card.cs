8de181d2aa68e0873803cad5ce99659ceb6aa915
bitpunkxr <tony69elvis@gmail.com>
2026-04-09 14:57:23 +0100

    Added Reusable Card component

--- Pages/Card.cs
+++ Pages/Card.cs
@@ -1,5 +1,5 @@
 public class Card {
     // public string Icon { get; set; }
-    public string Name { get; set; } = "";
+    public string Title { get; set; } = "";
     public string Info { get; set; } = "";
 }
